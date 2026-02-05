e3347b04922b6635f381742814d677c8f3f9e87c
Kay <jwcmckay@gmail.com>
2026-02-05 15:21:57 +0100

    Simple implementation of mandelbrot algorithm with plotting

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,13 +1,61 @@
-class mandelbrot(self):
+import numpy as np
+import matplotlib.pyplot as plt
 
+class mandelbrot:
 
-    def calculation(x):
+    def complex_matrix(self, xmin,xmax,ymin,ymax, density):
         """
-        Example function for ?
+        Function that creates the matrix of complex numbers with some density between the max and min values for x,y
+
+        xmin = int()
+        xmax = int()
+        ymin = int()
+        ymax = int()
+        
+        density = int()
+
+        Returns = vectors with with calues for height and width
+
         """
+        re = np.linspace(xmin,xmax, int((xmax-xmin) * density) )
+        im = np.linspace(ymin,ymax, int((ymax-ymin) * density) )
+        return re[np.newaxis, :] + im[:, np.newaxis] * 1j
 
-        # Todo: implement something
-        # Add an extra comment
-        pass
+
+    def is_stable(self, c, max_iter):
+        """
+        Function that goes over each point c in grid
+        """
+        z = 0
+        for _ in range(max_iter):
+            z = z ** 2 + c
+        return abs(z) <= 2
+
+        
+    def escape_time(self, c, max_iter):
+        z = np.zeros_like(c)
+        iteration_counts = np.zeros(c.shape)
+        
+        for i in range(max_iter):
+            mask = np.abs(z) <= 2  # Points that haven't escaped yet
+            z[mask] = z[mask] ** 2 + c[mask]
+            iteration_counts[mask] = i
+        
+        return iteration_counts
 
 
+if __name__ == "__main__":
+    mb = mandelbrot()
+    c = mb.complex_matrix(-2, 0.5, -1.5, 1.5, density=500)
+    iterations = mb.escape_time(c, max_iter=100)
+
+    # Plot with colormap
+    plt.figure(figsize=(10, 8))
+    plt.imshow(iterations, extent=[-2, 0.5, -1.5, 1.5], 
+               cmap='hot', origin='lower', interpolation='bilinear')
+    plt.colorbar(label='Iterations to escape')
+    plt.title('Mandelbrot Set')
+    plt.xlabel('Real')
+    plt.ylabel('Imaginary')
+    plt.savefig('mandelbrot_colormap.png', dpi=300)
+    plt.show()
